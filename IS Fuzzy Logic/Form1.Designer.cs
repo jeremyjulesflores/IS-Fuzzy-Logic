715d34559459d85e75a69ff3812e673f2c101eff
Jeremy Jules Flores <jeremyjules.flores@cit.edu>
2022-12-12 19:46:14 +0800

    Update

--- IS Fuzzy Logic/Form1.Designer.cs
+++ IS Fuzzy Logic/Form1.Designer.cs
@@ -28,13 +28,215 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Target Temperature";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(170, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 88);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Click for 1 Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(372, 169);
+            this.panel1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(40, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 88);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label11);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(409, 6);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(372, 169);
+            this.panel2.TabIndex = 6;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(270, 136);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 14;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(270, 99);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(270, 61);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(125, 136);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(125, 99);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(125, 61);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "AC Output :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Room Temperature :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Target Temperature :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(161, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Data";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(809, 194);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- IS Fuzzy Logic/Form1.cs
+++ IS Fuzzy Logic/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotFuzzy;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,14 +7,100 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace IS_Fuzzy_Logic
 {
     public partial class Form1 : Form
     {
+        FuzzyEngine fe;
+        MembershipFunctionCollection roomTemp, targetTemp, acOutput;
+        LinguisticVariable roomTempLevel, targetTempLevel, acOutputLevel;
+        FuzzyRuleCollection myrules;
+
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            setMembers();
+            setRules();
+        }
+        public void setMembers()
+        {
+
+            roomTemp = new MembershipFunctionCollection();
+            roomTemp.Add(new MembershipFunction("VERY_COLD", -15.0, -10.0, -10.0, -5.0));
+            roomTemp.Add(new MembershipFunction("COLD", -10.0, -5.0, -5.0, 0.0));
+            roomTemp.Add(new MembershipFunction("WARM", -5.0, 0.0, 0.0, 5.0));
+            roomTemp.Add(new MembershipFunction("HOT", 0.0, 5.0, 5.0, 10.0));
+            roomTemp.Add(new MembershipFunction("VERY_HOT", 5.0, 10.0, 10.0, 15.0));
+            roomTempLevel = new LinguisticVariable("ROOM", roomTemp);
+
+
+            targetTemp = new MembershipFunctionCollection();
+            targetTemp.Add(new MembershipFunction("VERY_COLD", -10.0, 0.0, 0.0, 10.0));
+            targetTemp.Add(new MembershipFunction("COLD", 0.0, 10.0, 10.0, 20.0));
+            targetTemp.Add(new MembershipFunction("WARM", 10.0, 20.0, 20.0, 30.0));
+            targetTemp.Add(new MembershipFunction("HOT", 20.0, 30.0, 30.0, 40.0));
+            targetTemp.Add(new MembershipFunction("VERY_HOT", 30.0, 40.0, 40.0, 50.0));
+            targetTempLevel = new LinguisticVariable("TARGET", targetTemp);
+
+            acOutput = new MembershipFunctionCollection();
+            acOutput.Add(new MembershipFunction("COOL_ALOT", -15.0, -10.0, -10.0, -5.0));
+            acOutput.Add(new MembershipFunction("COOL", -10.0, -5.0, -5.0, 0.0));
+            acOutput.Add(new MembershipFunction("NO_CHANGE", -5.0, 0.0, 0.0, 5.0));
+            acOutput.Add(new MembershipFunction("HEAT", 0.0, 5.0, 5.0, 10.0));
+            acOutput.Add(new MembershipFunction("HEAT_ALOT", 5.0, 10.0, 10.0, 15.0));
+            acOutputLevel = new LinguisticVariable("ACOUTPUT", acOutput);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fuziffyvalues();
+            defuzzy();
+        }
+
+        public void setRules()
+        {
+            myrules = new FuzzyRuleCollection();
+            myrules.Add(new FuzzyRule("IF (ROOM IS COLD) OR (ROOM IS VERY_COLD) AND (TARGET IS WARM) THEN ACOUTPUT IS HEAT"));
+            myrules.Add(new FuzzyRule("IF (ROOM IS HOT) OR (ROOM IS VERY_HOT) AND (TARGET IS WARM) THEN ACOUTPUT IS COOL"));
+            myrules.Add(new FuzzyRule("IF (ROOM IS WARM) AND (TARGET IS WARM) THEN ACOUTPUT IS NO_CHANGE"));
+        }
+
+        public void setFuzzyEngine()
+        {
+            fe = new FuzzyEngine();
+            fe.LinguisticVariableCollection.Add(roomTempLevel);
+            fe.LinguisticVariableCollection.Add(targetTempLevel);
+            fe.LinguisticVariableCollection.Add(acOutputLevel);
+            fe.FuzzyRuleCollection = myrules;
+        }
+
+        public void fuziffyvalues()
+        {
+            roomTempLevel.InputValue = 0.0;
+            roomTempLevel.Fuzzify("VERY_COLD");
+            targetTempLevel.InputValue = (Convert.ToDouble(textBox1.Text));
+            targetTempLevel.Fuzzify("VERY_COLD");
+
+        }
+        public void defuzzy()
+        {
+            setFuzzyEngine();
+            fe.Consequent = "ACOUTPUT";
+            label11.Text = "" + fe.Defuzzify();
+            label6.Text = textBox1.Text;
+
+            fe.Consequent = "ROOM";
+            label7.Text = "" + fe.Defuzzify();
+
+        }
+
     }
 }
