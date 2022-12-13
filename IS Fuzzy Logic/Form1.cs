656cbd19a1fc0883753aaf3ec2ea0134dfca3cd0
Jeremy Jules Flores <jeremyjules.flores@cit.edu>
2022-12-13 10:19:47 +0800

    Update

--- IS Fuzzy Logic/Form1.cs
+++ IS Fuzzy Logic/Form1.cs
@@ -29,6 +29,8 @@ namespace IS_Fuzzy_Logic
         {
             setMembers();
             setRules();
+            setFuzzyEngine();
+            label7.Text = "30.0";
         }
         public void setMembers()
         {
@@ -59,26 +61,65 @@ namespace IS_Fuzzy_Logic
 
         private void button1_Click(object sender, EventArgs e)
         {
+            label6.Text = textBox1.Text;
             fuziffyvalues();
             defuzzy();
+            computenewtemp();
         }
 
         public void setRules()
         {
             myrules = new FuzzyRuleCollection();
+            //IF ROOM IS COLD  AND VERY COLD
+            myrules.Add(new FuzzyRule("IF (ROOM IS COLD) AND (TARGET IS VERY_COLD) THEN ACOUTPUT IS COOL"));
+
+            myrules.Add(new FuzzyRule("IF (ROOM IS VERY_COLD) AND (TARGET IS COLD) THEN ACOUTPUT IS HEAT"));
+
             myrules.Add(new FuzzyRule("IF (ROOM IS COLD) AND (TARGET IS WARM) THEN ACOUTPUT IS HEAT"));
             myrules.Add(new FuzzyRule("IF (ROOM IS VERY_COLD) AND (TARGET IS WARM) THEN ACOUTPUT IS HEAT"));
 
+            myrules.Add(new FuzzyRule("IF (ROOM IS COLD) AND (TARGET IS HOT) THEN ACOUTPUT IS HEAT"));
+            myrules.Add(new FuzzyRule("IF (ROOM IS VERY_COLD) AND (TARGET IS HOT) THEN ACOUTPUT IS HEAT"));
+
+            myrules.Add(new FuzzyRule("IF (ROOM IS COLD) AND (TARGET IS VERY_HOT) THEN ACOUTPUT IS HEAT"));
+            myrules.Add(new FuzzyRule("IF (ROOM IS VERY_COLD) AND (TARGET IS VERY_HOT) THEN ACOUTPUT IS HEAT"));
+
+            //IF ROOM IS HOT AND VERY HOT
+            myrules.Add(new FuzzyRule("IF (ROOM IS HOT) AND (TARGET IS VERY_HOT) THEN ACOUTPUT IS HEAT"));
+
+            myrules.Add(new FuzzyRule("IF (ROOM IS VERY_HOT) AND (TARGET IS HOT) THEN ACOUTPUT IS COOL"));
 
             myrules.Add(new FuzzyRule("IF (ROOM IS HOT) AND (TARGET IS WARM) THEN ACOUTPUT IS COOL"));
             myrules.Add(new FuzzyRule("IF (ROOM IS VERY_HOT) AND (TARGET IS WARM) THEN ACOUTPUT IS COOL"));
 
+            myrules.Add(new FuzzyRule("IF (ROOM IS HOT) AND (TARGET IS COLD) THEN ACOUTPUT IS COOL"));
+            myrules.Add(new FuzzyRule("IF (ROOM IS VERY_HOT) AND (TARGET IS COLD) THEN ACOUTPUT IS COOL"));
 
+            myrules.Add(new FuzzyRule("IF (ROOM IS HOT) AND (TARGET IS VERY_COLD) THEN ACOUTPUT IS COOL"));
+            myrules.Add(new FuzzyRule("IF (ROOM IS VERY_HOT) AND (TARGET IS VERY_COLD) THEN ACOUTPUT IS COOL"));
+
+            //IF ROOM IS WARM
+            myrules.Add(new FuzzyRule("IF (ROOM IS WARM) AND (TARGET IS VERY_COLD) THEN ACOUTPUT IS COOL"));
+            myrules.Add(new FuzzyRule("IF (ROOM IS WARM) AND (TARGET IS COLD) THEN ACOUTPUT IS COOL"));
+            myrules.Add(new FuzzyRule("IF (ROOM IS WARM) AND (TARGET IS HOT) THEN ACOUTPUT IS HEAT"));
+            myrules.Add(new FuzzyRule("IF (ROOM IS WARM) AND (TARGET IS VERY_HOT) THEN ACOUTPUT IS HEAT"));
+
+
+
+            //NO CHANGE RULES
             myrules.Add(new FuzzyRule("IF (ROOM IS WARM) AND (TARGET IS WARM) THEN ACOUTPUT IS NO_CHANGE"));
             myrules.Add(new FuzzyRule("IF (ROOM IS COLD) AND (TARGET IS COLD) THEN ACOUTPUT IS NO_CHANGE"));
             myrules.Add(new FuzzyRule("IF (ROOM IS VERY_COLD) AND (TARGET IS VERY_COLD) THEN ACOUTPUT IS NO_CHANGE"));
             myrules.Add(new FuzzyRule("IF (ROOM IS VERY_HOT) AND (TARGET IS VERY_HOT) THEN ACOUTPUT IS NO_CHANGE"));
             myrules.Add(new FuzzyRule("IF (ROOM IS HOT) AND (TARGET IS HOT) THEN ACOUTPUT IS NO_CHANGE"));
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            label7.Text = "30.0";
+            label8.Text = "";
 
         }
 
@@ -93,21 +134,25 @@ namespace IS_Fuzzy_Logic
 
         public void fuziffyvalues()
         {
-            roomTempLevel.InputValue = 30.0;
-            roomTempLevel.Fuzzify("HOT");
+            roomTempLevel.InputValue = Convert.ToDouble(label7.Text);
+            roomTempLevel.Fuzzify("VERY_COLD");
+
             targetTempLevel.InputValue = (Convert.ToDouble(textBox1.Text));
-            targetTempLevel.Fuzzify("VERY_HOT");
+            targetTempLevel.Fuzzify("VERY_COLD");
 
         }
         public void defuzzy()
         {
-            setFuzzyEngine();
             fe.Consequent = "ACOUTPUT";
-            label11.Text = "" + fe.Defuzzify();
-            label6.Text = textBox1.Text;
+            label8.Text = "" + fe.Defuzzify();
+        }
 
-            fe.Consequent = "ROOM";
-            label7.Text = "" + fe.Defuzzify();
+        public void computenewtemp()
+        {
+            double oldtemp = Convert.ToDouble(label7.Text);
+            double oldacoutput = Convert.ToDouble(label8.Text);
+            double newtemp = oldtemp + oldacoutput;
+            label7.Text = newtemp.ToString(); ;
 
         }
 
